a48e54e095c98d01df12fb3d9d4ae3cc573d51b2
Tommaso Venza <tommasovenza@gmail.com>
2020-05-28 16:10:41 +0200

    esercizio 4 up

--- snack_4/js/script.js
+++ snack_4/js/script.js
@@ -3,3 +3,25 @@
 // può partecipare o no alla festa.
 
 
+// l'array degli invitati alla festa
+var arrayInvitati = ['tommaso', 'monica', 'ernesto', 'tizio', 'caio', 'sempronio'];
+
+// chiedo il nome all'utente
+var ask = prompt('qual è il tuo nome?');
+
+// imposto variabile di ingresso come falsa
+var ingresso = false;
+
+// ciclo for
+for (i = 0; i < arrayInvitati.length; i++) {
+
+    if (arrayInvitati[i] === ask ) {
+        ingresso = true;
+    }
+}
+
+if (ingresso) {
+    alert('sei invitato, entra!');
+} else {
+    alert('mi dispiace, non sei invitato!');
+}
