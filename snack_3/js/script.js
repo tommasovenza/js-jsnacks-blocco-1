5f8a3290dce8e6c1f7e27704035ba0f359b38d0f
Tommaso Venza <tommasovenza@gmail.com>
2020-05-28 15:52:18 +0200

    esercizio 3 up

--- snack_3/js/script.js
+++ snack_3/js/script.js
@@ -3,7 +3,15 @@
 // inseriti. Esegui questo programma in due versioni, con il for
 // e con il while.
 
+var somma = 0;
+for (i = 1; i <= 5; i++) {
 
+    var x = parseInt(prompt('chiedi un numero'));
+    console.log(x);
 
+    somma = somma + x;
+    
+}
 
+console.log(somma);
 
