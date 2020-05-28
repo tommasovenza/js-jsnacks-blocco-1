bef28cf291ede70e09ef29cd4b33c815d5b7bd1a
Tommaso Venza <tommasovenza@gmail.com>
2020-05-28 15:40:39 +0200

    esercizio 2 up

--- snack_2/js/script.js
+++ snack_2/js/script.js
@@ -3,5 +3,16 @@
 // parola più lunga.
 
 
+// le parole
+
+var parolaUno = prompt('Inserisci una parola');
+var parolaDue = prompt('Inserisci una parola');
 
 
+if (parolaUno.length < parolaDue.length) {
+    alert('la prima parola è più corta rispetto alla seconda')
+} else if (parolaDue.length === parolaUno.length) {
+    alert('le parole hanno la stessa lunghezza');
+} else {
+    alert('la seconda parola è più corta');
+}
