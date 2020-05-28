a9b46c9265516af0dc730d7e148bf3ebf6aa9aa1
Tommaso Venza <tommasovenza@gmail.com>
2020-05-28 15:28:07 +0200

    esercizio 1 up

--- snack_1/js/script.js
+++ snack_1/js/script.js
@@ -1,3 +1,15 @@
 // L’utente inserisce due numeri in successione, con due prompt. 
 // Il software stampa il maggiore.
 
+// i due numeri
+var numeroUno = parseInt(prompt('inserisci un numero'));
+var numeroDue = parseInt(prompt('inserisci un numero'));
+
+if (numeroUno > numeroDue) {
+    alert('il primo numero è maggiore');
+} else if (numeroDue === numeroUno ) {
+    alert('i numeri sono uguali');
+} else {
+    alert('il secondo numero è maggiore');
+}
+
