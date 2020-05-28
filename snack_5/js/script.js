dd087ff912ca4708fa17bd4d516382e655f0fac1
Tommaso Venza <tommasovenza@gmail.com>
2020-05-28 16:51:59 +0200

    esercizio 5 up

--- snack_5/js/script.js
+++ snack_5/js/script.js
@@ -2,5 +2,28 @@
 // di tutte le cifre che compongono il numero.
 
 
+var numero = parseInt(prompt('inserisci un numero di quattro cifre'));
+
+while (numero === !isNaN)  {
+    var numero = parseInt(prompt('inserisci un numero di quattro cifre'));
+
+}
+
+var numeroStringa = numero.toString();
+
+var x = numeroStringa.split('');
+
+console.log(x);
+
+var somma = 0;
+for (i = 0; i < x.length; i++) {
+
+    y = parseInt(x[i]);
+
+    somma = somma + y;
+
+}
+
+alert('la somma è dei numeri inseriti è ' + somma);
 
 
